6c3d7b3e1f6afd2f9ae9aea685fdbc726a31a2a5
kaitlyn <kaitlyn_williams@brown.edu>
2024-03-14 18:46:00 -0400

    can u see my changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -12,17 +13,32 @@ function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
 
+  const [cart, setCart] = useState(0)
+  const inc = () => {setCart + bakeryData.price}
+  const addToCart = (item) => {
+    setCart([...cart, item]); // Adding the new item to the cart
+  };
+
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <p key={index}>
+        <button onClick={() => addToCart(item)}>Add to Cart</button>
+        {item.name}
+      </p> // replace with BakeryItem component
       ))}
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        <ul>
+          {cart.map((item, index) => (
+            <li key={index}>{item.name}</li>
+          ))}
+        </ul>
+        {/* TODO: render a list of items in the cart */
+        bakeryData.name}
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,15 @@
 // TODO: create a component that displays a single bakery item
+
+
+export default function BakeryItem({item}) {
+    return (
+        <div className="bakery-item">
+        <img src={item.image} alt={item.name} />
+        <h3>{item.name}</h3>
+        <p>{item.description}</p>
+        <p>Price: ${item.price}</p>
+        <button>Add to Cart</button>
+      </div>
+    )
+}
+  
